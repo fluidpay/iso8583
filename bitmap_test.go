70af6f929c261375d507a018853188b690bc1a86
PumpkinSeed <qwer.kocka@gmail.com>
2020-05-14 17:57:23 +0200

    Add bitmap handler

--- bitmap_test.go
+++ bitmap_test.go
@@ -2,8 +2,49 @@ package iso8583
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
+
+func TestAddField(t *testing.T) {
+	var scenarios = []struct {
+		result string
+		bits   []uint8
+	}{
+		{
+			result: "42000104021C0078",
+			bits:   []uint8{2, 7, 24, 30, 39, 44, 45, 46, 58, 59, 60, 61},
+		},
+		{
+			result: "4200010002140068",
+			bits:   []uint8{2, 7, 24, 39, 44, 46, 58, 59, 61},
+		},
+		{
+			result: "0000010002140068",
+			bits:   []uint8{24, 39, 44, 46, 58, 59, 61},
+		},
+		{
+			result: "A888010002143068",
+			bits:   []uint8{1, 3, 5, 9, 13, 24, 39, 44, 46, 51, 52, 58, 59, 61},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		var fields uint64
+		for _, bit := range scenario.bits {
+			fields = addField(fields, bit)
+		}
+		if result := bitmapHex(fields); scenario.result != result {
+			t.Errorf("bitmap should be %s, instead of %s", scenario.result, result)
+		}
+	}
+}
+
+func TestLeftPad(t *testing.T) {
+	data := "923234"
+	res := leftPad(data, 12, "0")
+	fmt.Println(res)
+}
 
 func TestBitmapExUnpacked(t *testing.T) {
 	fields := make(map[int]struct{})
@@ -187,6 +228,3 @@ func TestBitmapPurchaseWithCashBackSample(t *testing.T) {
 		t.Error("not equal")
 	}
 }
-
-
-
