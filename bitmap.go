5a02dd6232322ccdeeb0bd8a58844bc9dff71abd
hajnalandor <andor.hajnal@fluidpay.com>
2020-05-18 09:26:43 +0200

    remove old bitmap handler

--- bitmap.go
+++ bitmap.go
@@ -1,7 +1,6 @@
 package iso8583
 
 import (
-	"encoding/hex"
 	"fmt"
 )
 
@@ -19,27 +18,4 @@ func bitmapHex(fields uint64) string {
 	// %016X will convert the fields (uint64) to hexadecimal number
 	// with left padding to length of 16
 	return fmt.Sprintf("%016X", fields)
-}
-
-//TODO unpacked format
-func encodeBitmap(fields map[int]struct{}, packed, secondaryBitmap bool) ([]byte, error) {
-	byteNum := 8
-	if secondaryBitmap {
-		byteNum = 16
-	}
-	b := make([]byte, byteNum)
-	if secondaryBitmap {
-		b[0] = b[0] | 1<<7 //if secondary encodeBitmap is present, first bit in byte[0] is 1
-	}
-
-	for i, _ := range fields {
-		if _, ok := fields[i]; ok {
-			byteInd := (i - 1) / 8
-			b[byteInd] = b[byteInd] | 1<<((8-i%8)%8)
-		}
-	}
-	if packed {
-		return hex.DecodeString(fmt.Sprintf("%X", b))
-	}
-	return b, nil
 }
